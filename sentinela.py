7e53a362ea8e9f16b1cee2e102251a8db97dc4ab
Owyll <wyll82160@gmail.com>
2026-04-29 14:08:01 +0000

    v.1

--- sentinela.py
+++ sentinela.py
@@ -1,1 +1,6 @@
-
+#--Variavel Sentinela--
+while True:
+    comando = input("Digite o Comando, Se precisar Parar Digite:Sair")
+    if comando == "sair" :
+        break
+    print(f"Executando:{comando}")
