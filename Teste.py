554b7282232b9d02309c652266f5f6a02f562c37
Owyll <wyll82160@gmail.com>
2026-05-13 14:25:17 +0000

    v1

--- Teste.py
+++ Teste.py
@@ -1,1 +1,21 @@
-
+#Criando um Teste com Pontuação--
+print("Teste seus Conhecimentos sobre ciências🔢")
+pontos = 0
+#--Questão 1--
+print("Qual é a formula da água?♒")
+print("a- H20 \n b- c02 \n c-A1")
+resposta1 = input("Digite sua Resposta:  ").lower()
+if resposta1 == "a":
+    print("Resposta Correta👌")
+    pontos = pontos = +1
+else:
+    print("Você Errou!")
+#Questão 2 
+print("2- O sol é:")
+print("a- um satélite \n b- uma estrela \n c- um asteróide")
+resposta2 = input("Digite sua resposta: ").lower()
+if resposta2== "b":
+    print("Está Correto👌")
+    pontos == pontos +1
+else:
+    print("Você errou!")
